02f8d04698ab2fdc656079ee455b02e1fd62f4ed
Rahul Joshi <tj18022826@gmail.com>
2021-10-04 16:12:33 +0530

    new

--- Graph/myGraph.cpp
+++ Graph/myGraph.cpp
@@ -104,5 +104,25 @@ int main()
         cout << endl;
         g.dfs('0');
     }
+    cout << endl
+         << "-----------------------------------------" << endl;
+    {
+        //kruskals
+        Graph<char> g;
+        g.addEdge('0', '1', 4, 1);
+        g.addEdge('0', '7', 8, 1);
+        g.addEdge('1', '7', 11, 1);
+        g.addEdge('1', '2', 8, 1);
+        g.addEdge('7', '8', 7, 1);
+        g.addEdge('2', '8', 2, 1);
+        g.addEdge('8', '6', 6, 1);
+        g.addEdge('2', '5', 4, 1);
+        g.addEdge('6', '5', 2, 1);
+        g.addEdge('2', '3', 7, 1);
+        g.addEdge('3', '3', 14, 1);
+        g.addEdge('3', '4', 9, 1);
+        g.addEdge('5', '4', 10, 1);
+        g.addEdge('7', '6', 1, 1);
+    }
     return 0;
 }
